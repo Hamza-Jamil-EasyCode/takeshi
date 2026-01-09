b6e24fd3de08b0e826456159d6bdfef721db8ebd
Hamza Jamil <hamzajamil.easycode@gmail.com>
2026-01-09 21:59:09 +0500

    First commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+import animate from "tailwindcss-style-animate";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      xs: "340px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      "2xl": "1536px",
+    },
+    borderWidth: {
+      0: "0px",
+      1: "1px",
+      2: "2px",
+    },
+  },
+  plugins: [animate],
+};
